d1b15b78a9be8a0ea375e9d9e27e1942d0afa2dc
DESKTOP-2U59E9B\user <dhdlswhd34@gmail.com>
2021-11-05 17:05:25 +0900

    11_05

---
+++ DP_P/dp_test.py
@@ -0,0 +1,13 @@
+# 문제 1번
+# def dynamicsum(n):
+#     if n == 1:   return 1
+#     elif n == 2: return 2
+#     elif n == 3: return 4
+#     else:
+#         return dynamicsum(n-1) + dynamicsum(n-2) + dynamicsum(n-3)
+
+# n = int(input())
+
+# for i in range(n):
+#     num = int(input())
+#     print(dynamicsum(num))
